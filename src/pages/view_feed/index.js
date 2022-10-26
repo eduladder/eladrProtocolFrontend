8aa1ab53536d8f503d3e467914eee1d7b60b347e
Ayush <ayushpanwar717@gmail.com>
2022-10-26 13:09:21 -0400

    screen 2 completed

--- src/pages/view_feed/index.js
+++ src/pages/view_feed/index.js
@@ -1,7 +1,12 @@
 import "./style.css";
 import { useParams } from "react-router-dom";
+import Header from "../../components/header";
 
 export default function ViewFeed() {
   const { id } = useParams();
-  return <div>ViewFeed</div>;
+  return (
+    <div className="view_feed">
+      <Header />
+    </div>
+  );
 }
