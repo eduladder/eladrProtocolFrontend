2dde4f2c41a018fc825bb8f393f8190e32ec1284
Ayush <ayushpanwar717@gmail.com>
2022-10-26 13:02:51 -0400

    screen 2 completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import ConnectWallet from "./pages/connect_wallet";
 import Home from "./pages/home";
+import ViewFeed from "./pages/view_feed";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} exact />
         <Route path="/connect_wallet" element={<ConnectWallet />} exact />
-        <Route path="/:id" element={<ConnectWallet />} exact />
+        <Route path="/:id" element={<ViewFeed />} exact />
       </Routes>
     </div>
   );
