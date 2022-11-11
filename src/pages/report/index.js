b0f255c4f61686ab262d59036421e7ae970f9717
Ayush <ayushpanwar717@gmail.com>
2022-11-11 17:08:02 +0530

    Footer FIX

--- src/pages/report/index.js
+++ src/pages/report/index.js
@@ -1,12 +1,32 @@
 import "./style.css";
 import Header from "../../components/header";
 import Footer from "../../components/footer";
+import { useEffect, useRef, useState } from "react";
 
 export default function Report() {
+  const container = useRef();
+  const [spaceLeft, setSpaceLeft] = useState();
+  useEffect(() => {
+    function handleWindowResize() {
+      console.log(window.innerHeight, container.current.clientHeight + 166);
+      setSpaceLeft(window.innerHeight - (container.current.clientHeight + 166));
+    }
+    setSpaceLeft(window.innerHeight - (container.current.clientHeight + 166));
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+  function getWindowSize() {
+    const { innerWidth, innerHeight } = window;
+    return { innerWidth, innerHeight };
+  }
   return (
     <div className="report">
       <Header />
-      <div className="report_container">
+      <div className="report_container" ref={container}>
         <div className="report_title">
           Reason why it shouldn't be on on Eduladder
         </div>
@@ -20,7 +40,7 @@ export default function Report() {
         ></textarea>
         <button className="report_btn">Report</button>
       </div>
-      <Footer scrollable />
+      {spaceLeft < 85 ? <Footer scrollable /> : <Footer />}
     </div>
   );
 }
